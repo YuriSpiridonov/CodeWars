e41ff116f2163a3ab88df544d52d81d29629f296
Yuri <48239309+YuriSpiridonov@users.noreply.github.com>
2019-03-11 15:08:54 +0300

    Update ease-the-stockbroker.py

--- ease-the-stockbroker.py
+++ ease-the-stockbroker.py
@@ -1,13 +1,19 @@
 # https://www.codewars.com/kata/ease-the-stockbroker/train/python
 # IN PROGRESS lots of issues, doing everything wrong
+import re
 
 def balance_statement(lst):
-    lst1 = lst.split(', ')
     lst2 = list()
     badFormat = list()
+    testlist = list()
+    lst1 = lst.split(', ')
     for x in range(len(lst1)):
         lst2.append(lst1[x].split(' '))
+    #testlist = re.findall('\S+',lst) return same as lines 8 to 10 but not useble format    
     print(lst2)
+    # fist need to find way check str content for inner value is lst2[1] is able to be int and [2] to be float
+    # my try re for it [1] only digits [2] digits with a dot . then convert it to int and float
+    
     for i in range(len(lst2)):
         lst2[i].insert(2, int(lst2[i][1]))
         lst2[i].pop(1)
