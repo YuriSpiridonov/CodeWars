ab5fe82cf4a78e9e4eaadd52d9ad01e84d584d79
Yuri <48239309+YuriSpiridonov@users.noreply.github.com>
2019-04-25 11:39:24 +0300

    Update regex-validate-pin-code.py

--- regex-validate-pin-code.py
+++ regex-validate-pin-code.py
@@ -3,9 +3,9 @@
 
 import re
 def validate_pin(pin):
-    regex = re.compile(r'\b\d{4}\b|\b\d{6}\b')
+    regex = re.compile(r'^\d{4}$|^\d{6}$')
     mo = regex.match(pin)
-    if mo:
+    if mo and mo.end() == len(pin):
         return True
     else:
         return False
