f019b7ff4a1e49ea0a41d3aff2b560dc48a380b2
Yuri <48239309+YuriSpiridonov@users.noreply.github.com>
2019-07-10 18:20:19 +0300

    5 kyu

--- molecule-to-atoms.py
+++ molecule-to-atoms.py
@@ -1,43 +1,36 @@
 # https://www.codewars.com/kata/molecule-to-atoms/train/python
-# all wrong check after resolve!
 
 import re
-def parse_molecule (formula):
-    moleculaDict = dict()
-    regex = re.compile(r'''
-    ([A-Z][a-z]?)(\d)?#((?<=\))\d)?((?<=\])\d)?
-    ''',re.VERBOSE)
-    mo = regex.findall(formula)
-    print(mo)
-    bracketsRegex = re.compile(r'''
-    \(([A-Z][a-z]?)
-    ''',re.VERBOSE)
-    mo2 =  bracketsRegex.findall(formula)
-    bracketsRegex2 = re.compile(r'''
-    \[(.*)\](\d)
-    ''',re.VERBOSE)
-    mo3 =  bracketsRegex2.search(formula)
-    print(mo2)
-    for i in range(1,len(mo)+1):
-        if mo[-i][1] == '':
-            mo[-i] = (mo[-i][0], 1)
+def replacer(match):
+    regex = re.compile(r'([A-Z][a-z]?)(\d+)?')
+    mo = regex.findall(match)
+    number = re.search(r'(?<=[\)\]\}])\d', match)
+    match = ''
+    for i in range(len(mo)):
+        if number is None:
+            match += mo[i][0]+mo[i][1]
             continue
+        if mo[i][1].isdigit() is False:
+            match += mo[i][0]+str(1*int(number.group()))
         else:
-            continue
-    for i in range(1,len(mo)+1):    
-        if mo2:
-            print(mo2.group(2))
-            if mo[-i][0] in mo2.group():
-                print(mo[-i][0])
-                mo[-i] = (mo[-i][0], int(mo[-i][1])*int(mo2.group(2)))
-                moleculaDict[mo[-i][0]] = 0+int(mo[-i][1])*int(mo2.group(2))
-    for element in mo:
-        moleculaDict[element[0]] = moleculaDict.get(element[0], element[1])
-        if element[1]:
-            moleculaDict[element[0]] = int(element[1])
-            
+            match += mo[i][0]+str(int(mo[i][1])*int(number.group()))        
+    return match
     
-        
-print(parse_molecule("K4[ON(SO3)2]2"))#, {'K': 4,  'O': 14,  'N': 2,  'S': 4}), "Should parse Fremy's salt: K4[ON(SO3)2]2")
-#print(parse_molecule("H2O"))    
-#print(parse_molecule("Mg(OH)2"))#, {'Mg': 1, 'O' : 2, 'H': 2}), "Should parse magnesium hydroxide: Mg(OH)2")
+def parse_molecule(formula):
+    moleculaDict = dict()
+    formula = re.sub(r'\(.*?\)\d?', lambda mo: replacer(mo.group()), formula)
+    formula = re.sub(r'\[.*?\]\d?', lambda mo: replacer(mo.group()), formula)
+    formula = re.sub(r'\{.*?\}\d?', lambda mo: replacer(mo.group()), formula)
+    regex = re.compile(r'([A-Z][a-z]?)(\d+)?')
+    mo = regex.findall(formula)
+    for element in mo:
+        if element[0] in moleculaDict.keys():
+            if element[1].isdigit() is False:
+                moleculaDict[element[0]] += 1 
+                continue
+            moleculaDict[element[0]] += int(element[1])
+        if element[1].isdigit() is False:
+            moleculaDict[element[0]] = moleculaDict.get(element[0], 1)
+            continue
+        moleculaDict[element[0]] = moleculaDict.get(element[0], int(element[1]))
+    return moleculaDict
